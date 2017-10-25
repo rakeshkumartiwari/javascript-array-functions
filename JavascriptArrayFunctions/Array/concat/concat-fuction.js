eefe82a93eb3f31c20ce2f163b7f5d585787476d
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2017-10-26 01:16:15 +0530

    array functions.

---
+++ JavascriptArrayFunctions/Array/concat/concat-fuction.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+
+    $btnList = $("#btnList");
+    $btnNewList = $("#btnNewList");
+    $lblResult = $("#lblResult");
+
+    var people = [
+
+        { name: "Rakesh" }, { name: "Ritesh" }, { name: "Rupesh" }, { name: "Rajnish" }
+    ];
+    var people1 = [
+        { name: "Shivam" }, { name: "Rishi" }
+    ];
+
+    $btnList.click(function () {
+        var lblResult = "";
+        $lblResult.html("");
+        $.each(people, function () {
+            lblResult += this.name + "<br>";
+        });
+        $lblResult.html(lblResult)
+
+    });
+
+
+    $btnNewList.click(function () {
+        var lblResult = "";
+        $lblResult.html("");
+
+        // here i used concat function
+        $.each(people.concat(people1), function () {
+            lblResult += this.name + "<br>";
+        });
+        $lblResult.html(lblResult)
+
+    });
+});
